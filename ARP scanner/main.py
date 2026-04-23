cb820fe3adf1e9474378737dafc2c11ec2e60368
Nihnio <danieladewoleayo31651@gmail.com>
2026-04-23 18:58:56 +0100

    Renamed IP address

--- ARP scanner/main.py
+++ ARP scanner/main.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     broadcast = "FF:FF:FF:FF:FF:FF:FF:FF" # send packet to all the devices in the network
     ether_layer = Ether(dst=broadcast) #set source & destination mac address
  
-    IP_range = "192.168.173.0/24" #range of ip addresses/host devices to scan
+    IP_range = "192.168.1.0/24" #range of ip addresses/host devices to scan
     ARP_layer = ARP(pdst = IP_range) #set arp request & target ip address
 
     packet = ether_layer/ARP_layer #create packet with both layers
